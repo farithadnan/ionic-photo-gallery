54265d6dca04d73adb5cf44dc26b94abb69c3044
Farith Adnan <ayiedfarith@gmail.com>
2022-05-19 16:49:04 +0800

    Loading photos functionality

--- src/app/services/photo.service.ts
+++ src/app/services/photo.service.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/naming-convention */
 import { Camera, CameraResultType, CameraSource, Photo } from '@capacitor/camera';
 import { Filesystem, Directory } from '@capacitor/filesystem';
 import { Storage } from '@capacitor/storage';
@@ -9,6 +10,8 @@ import { Injectable } from '@angular/core';
 })
 export class PhotoService {
   public photos: UserPhoto[] = [];
+  // eslint-disable-next-line @typescript-eslint/no-inferrable-types
+  private PHOTO_STORAGE: string = 'photos';
 
   constructor() { }
 
@@ -24,6 +27,31 @@ export class PhotoService {
     // Save the picture and add it to photo collection
     const savedImageFile: any = await this.savePicture(capturedPhoto);
     this.photos.unshift(savedImageFile);
+
+    // Storage.set() to save the Photos array
+    // the Photos array is stored each time a new photo is taken.
+    Storage.set({
+      key: this.PHOTO_STORAGE,
+      value: JSON.stringify(this.photos),
+    });
+  }
+
+  public async loadSaved() {
+    // Retrieve cached photo array data
+    const photoList = await Storage.get({ key: this.PHOTO_STORAGE });
+    this.photos = JSON.parse(photoList.value) || [];
+
+    // Display the photo by reading into base64 format
+    for (const photo of this.photos) {
+      // Read each saved photo's data from the Filesystem
+      const readFile = await Filesystem.readFile({
+        path: photo.filepath,
+        directory: Directory.Data,
+      });
+
+      // Web platform only: Load the photo as base64 data
+      photo.webviewPath = `data:image/jpeg;base64,${readFile.data}`;
+    }
   }
 
   private async savePicture(photo: Photo) {
--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,14 +1,18 @@
 import { PhotoService } from '../services/photo.service';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss']
 })
-export class Tab2Page {
+export class Tab2Page implements OnInit {
 
   constructor(public photoService: PhotoService) {}
+
+  async ngOnInit() {
+    await this.photoService.loadSaved();
+  }
 
   addPhotoToGallery() {
     this.photoService.addNewToGallery();
